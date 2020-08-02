b67f7813662db4e80a88809ad425bde280c02823
Kom Sihon <rsihon@gmail.com>
2020-08-02 23:37:38 +0100

    30s song preview generated when song file is uploaded

--- mediastore/models.py
+++ mediastore/models.py
@@ -1,10 +1,25 @@
+import os
+import subprocess
+
+from django.conf import settings
 from django.contrib.humanize.templatetags.humanize import intcomma
 from django.db import models
 from djangotoolbox.fields import ListField
 
-from ikwen.core.models import Model, AbstractWatchModel
-from ikwen.core.fields import MultiImageField
-from ikwen.accesscontrol.models import Member
+from ikwen.core.models import AbstractWatchModel
+from ikwen.core.fields import MultiImageField, EventFileField
+
+
+def generate_song_preview(filename):
+    media_root = getattr(settings, 'MEDIA_ROOT')
+    previews_folder = media_root + "previews/songs/"
+    if not os.path.exists(previews_folder):
+        os.makedirs(previews_folder)
+    preview = media_root + "previews/" + filename.replace(media_root, '')
+    from ikwen.core.log import ikwen_error_log_filename
+    eh = open(ikwen_error_log_filename, 'a')
+    command = "ffmpeg -ss 00:00:00 -i %s -t 00:00:30 -c copy %s" % (filename, preview)
+    subprocess.call(command.split(' '), stderr=eh)
 
 
 class Artist(AbstractWatchModel):
@@ -70,7 +85,7 @@ class Song(AbstractWatchModel):
     slug = models.SlugField(db_index=True)
     cover = MultiImageField(upload_to='songs_covers', blank=True, null=True)
     preview = models.FileField(upload_to='song_previews', blank=True, null=True)
-    media = models.FileField(upload_to='songs', blank=True, null=True)
+    media = EventFileField(upload_to='songs', blank=True, null=True, callback=generate_song_preview)
     download_link = models.URLField(blank=True, null=True)
     cost = models.IntegerField(default=0)
     is_active = models.BooleanField(default=True)
